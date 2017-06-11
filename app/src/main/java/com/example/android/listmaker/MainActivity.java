126035b5e91947743a3727bc71fba99eb8a1b5e2
venkywonka <cyber.venks@gmail.com>
2017-06-11 15:34:59 +0530

    1.put some of the code inside the addListItem() function 2.used onCLickListener interface instead of onItemClickListener

--- app/src/main/java/com/example/android/listmaker/MainActivity.java
+++ app/src/main/java/com/example/android/listmaker/MainActivity.java
@@ -7,46 +7,47 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> list = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    ArrayList<String> list = new ArrayList<String>();
 
     public void addListItem(View view){
         EditText et = (EditText) findViewById(R.id.edit_text);
         list.add(et.getText().toString());
         et.setText("");
 
+        final ListView listView = (ListView) findViewById(R.id.list_view);
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
 
+        listView.setAdapter(itemsAdapter);
+
+        listView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                // Create a new intent to open the {@link SelectedListItem activity}
+                Intent numbersIntent = new Intent(MainActivity.this, SelectedListItem.class);
+                TextView t = (TextView) listView.getSelectedItem();
+                String msg = t.getText().toString();
+                numbersIntent.putExtra("blah",msg);
+
+                // Start the new activity
+                startActivity(numbersIntent);
+            }
+        });
     }/** above function called when ADD button is clicked, copies the listitem from EditText to list*/
 
-    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list);
-
-    ListView listView = (ListView) findViewById(R.id.list);
-
-     listView.setAdapter(arrayAdapter);
-
-    listView.setOnItemClickListener(new OnItemClickListener() {
-        @Override
-        public void onItemClick(ListView listView,View view,int pos,long id){
-            // Create a new intent to open the {@link SelectedListItem activity}
-            Intent numbersIntent = new Intent(MainActivity.this, SelectedListItem.class);
-            numbersIntent.putExtra("blah",list.get(pos));
-
-            // Start the new activity
-            startActivity(numbersIntent);
-        }
-    });
 
 
 }
